7041826d0b5f171952c970b888d77bf764be8966
siprianotrader <150560820+siprianotrader@users.noreply.github.com>
2025-02-16 13:50:07 -0400

    Atualização Final

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,11 +4,11 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://siprianotrader:2VDPNAfC6S9HA3gz@cluster0.lbcmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
-export const db = client.db("spotifyAula");
+export const db = client.db("sound");
 // const songCollection = await db.collection("songs").find({}).toArray();
 
 // console.log(songCollection);
